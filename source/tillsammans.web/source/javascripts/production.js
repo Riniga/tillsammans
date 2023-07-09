f0a2b8659eaa111782d9ba0bd5b03943201278eb
Rickard Nisses-Gagnér <rickard@nisses-gagner.se>
2023-07-09 22:26:32 +0200

    updated function urls

--- source/tillsammans.web/source/javascripts/production.js
+++ source/tillsammans.web/source/javascripts/production.js
@@ -1,4 +1,4 @@
-var loginApiUrl = "https://tillsammans.azurewebsites.net/api/Login?code=xPlWl5QzP7eP0fJqpLRjhSsvc0HwwWbjFZeCIq5uhUy3AzFuWXes1A==";
-var logoutApiUrl = "https://tillsammans.azurewebsites.net/api/Logout?code=xPlWl5QzP7eP0fJqpLRjhSsvc0HwwWbjFZeCIq5uhUy3AzFuWXes1A==";
+var loginApiUrl = "https://tillsammans.azurewebsites.net/api/Login?code=2hKZASicS1Bz5OU7Le9vgE8ZyIU8ro5O2qOpstsAvEqdAzFuyWvebw==";
+var logoutApiUrl = "https://tillsammans.azurewebsites.net/api/Logout?code=Hs-VBYeaX4wShHm___SBQvKGgCI9CocwTA2QfBt_Sx5JAzFuU2JR4g==";
 var readUserApiUrl = "https://tillsammans.azurewebsites.net/api/ReadUser?code=xPlWl5QzP7eP0fJqpLRjhSsvc0HwwWbjFZeCIq5uhUy3AzFuWXes1A==&";
-var readAllUsersApiUrl = "https://tillsammans.azurewebsites.net/api//ReadAllUser?code=xPlWl5QzP7eP0fJqpLRjhSsvc0HwwWbjFZeCIq5uhUy3AzFuWXes1A==";
+var readAllUsersApiUrl = "https://tillsammans.azurewebsites.net/api/ReadAllUser?code=pnytrHpd9DMMqS-srR5z6-RbnIVPQtwSjhSV80gPTtRuAzFuzHrGLw==";
